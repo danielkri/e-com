b2e037540d963a614e85f058c75d7c2e582d45d7
danielkri <dlkn87@gmail.com>
2020-03-25 12:33:26 +0200

    Updates

---
+++ precache-manifest.9534f7881848542953db967f8e0a3966.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33d24e5836d310c6d3f04fdea8e69815",
+    "url": "/e-com/index.html"
+  },
+  {
+    "revision": "3c4feb5b91097858b3ba",
+    "url": "/e-com/static/css/main.69465558.chunk.css"
+  },
+  {
+    "revision": "047aedf63a437375af06",
+    "url": "/e-com/static/js/2.c9ddefa2.chunk.js"
+  },
+  {
+    "revision": "f9416c2fa5dace694260608407c8cf2e",
+    "url": "/e-com/static/js/2.c9ddefa2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c4feb5b91097858b3ba",
+    "url": "/e-com/static/js/main.bbea1ba1.chunk.js"
+  },
+  {
+    "revision": "7fd1768777237fe99673",
+    "url": "/e-com/static/js/runtime-main.a9b48135.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/e-com/static/media/logo.ed28accb.svg"
+  }
+]);
