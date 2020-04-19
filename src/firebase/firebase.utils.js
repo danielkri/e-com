c5407c5c11046c170ccbb01838deca9104b783ff
danielkri <dlkn87@gmail.com>
2020-04-19 12:23:25 +0300

    moved data to firestore and added spinner
    moved shop data to firestore and added a spinner while loading data

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,8 @@ const config = {
     measurementId: "G-4TS36Q5QJF"
   };
 
+  firebase.initializeApp(config);
+
   export const createUserProfileDocument = async (userAuth, additionalData) => {
       if (!userAuth) return;
 
@@ -38,10 +40,38 @@ const config = {
       return userRef;
   };
 
-  firebase.initializeApp(config);
-
   export const auth = firebase.auth();
   export const firestore = firebase.firestore();
+
+  export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+    const collectionRef = firestore.collection(collectionKey);
+
+    const batch = firestore.batch();
+    objectsToAdd.forEach(obj => {
+      const newDocRef = collectionRef.doc();
+      batch.set(newDocRef, obj);
+    });
+
+    return await batch.commit();
+  };
+
+  export const convertCollectionsSnapshotToMap = (collections) => {
+    const transformedCollection = collections.docs.map(doc => {
+      const { title, items } = doc.data();
+
+      return {
+        routeName: encodeURI(title.toLowerCase()),
+        id: doc.id,
+        title,
+        items,
+      };
+    });
+
+    return transformedCollection.reduce((accumulator, collection) =>{
+      accumulator[collection.title.toLowerCase()] = collection;
+      return accumulator;
+    }, {});
+  }
 
   const provider = new firebase.auth.GoogleAuthProvider();
   provider.setCustomParameters({prompt: 'select_account'});
