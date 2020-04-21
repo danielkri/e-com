e7c8f0c077dad2564d185298830f6ffe0b3c3006
danielkri <dlkn87@gmail.com>
2020-04-21 19:24:02 +0300

    asynchronous redux and container pattern
    moved asynchronous calls to redux and implemented the container pattern on the shop components collection and collections-overview

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,6 +1,44 @@
-import { ShopActionTypes } from './shop.types';
+import { ShopActionTypes } from "./shop.types";
 
-export const updateCollections = (collectionsMap) => ({
-    type: ShopActionTypes.UPDATE_COLLECTIONS,
-    payload: collectionsMap,
-})
+import {
+  firestore,
+  convertCollectionsSnapshotToMap,
+} from "../../firebase/firebase.utils";
+
+export const fetchCollectionsStart = (collectionsMap) => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_START,
+  payload: collectionsMap,
+});
+
+export const fetchCollectionsSuccess = (collectionsMap) => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_SUCCESS,
+  payload: collectionsMap,
+});
+
+export const fetchCollectionsFailure = (errorMessage) => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
+  payload: errorMessage,
+});
+
+export const fetchCollectionsStartAsync = () => {
+  return (dispatch) => {
+    const collectionRef = firestore.collection("collections");
+
+    // fetch(
+    //   "https://firestore.googleapis.com/v1/projects/e-com-d-b/databases/(default)/documents/collections"
+    // )
+    //   .then((response) => response.json())
+    //   .then((collections) => console.log(collections));
+
+    dispatch(fetchCollectionsStart());
+
+    collectionRef
+      .get()
+      .then((snapshot) => {
+        const collectionMap = convertCollectionsSnapshotToMap(snapshot);
+        // updateCollections(collectionMap);
+        dispatch(fetchCollectionsSuccess(collectionMap));
+      })
+      .catch((error) => dispatch(fetchCollectionsFailure(error.message)));
+  };
+};
