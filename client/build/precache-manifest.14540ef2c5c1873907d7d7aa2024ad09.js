542fa214334f1ee89666e3bccb290ad02f33103f
danielkri <dlkn87@gmail.com>
2020-04-30 13:09:45 +0300

    added express server
    separating the cra app into client folder, added express server to help handle backend  stripe payment.

---
+++ client/build/precache-manifest.14540ef2c5c1873907d7d7aa2024ad09.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbed12a9d4733fd6ccc9bc407f5d3db9",
+    "url": "/e-com/index.html"
+  },
+  {
+    "revision": "a263e497c18dd582caf7",
+    "url": "/e-com/static/css/main.69465558.chunk.css"
+  },
+  {
+    "revision": "047aedf63a437375af06",
+    "url": "/e-com/static/js/2.c9ddefa2.chunk.js"
+  },
+  {
+    "revision": "f9416c2fa5dace694260608407c8cf2e",
+    "url": "/e-com/static/js/2.c9ddefa2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a263e497c18dd582caf7",
+    "url": "/e-com/static/js/main.1b39c859.chunk.js"
+  },
+  {
+    "revision": "7fd1768777237fe99673",
+    "url": "/e-com/static/js/runtime-main.a9b48135.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/e-com/static/media/logo.ed28accb.svg"
+  }
+]);
