45d3f8740d233ea274799ef78debda98d2a230c0
danielkri <dlkn87@gmail.com>
2020-05-07 19:35:29 +0300

    ErrorBoundary component
    adding lazy load and suspende and ErrorBoundary component

---
+++ client/src/components/error-boundary/error-boundary.component.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+import {
+  ErrorImageOverlay,
+  ErrorImageContainer,
+  ErrorImageText,
+} from "./error-boundary.styles";
+
+class ErrorBoundary extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      hasErrored: false,
+    };
+  }
+  static getDerivedStateFromError(error) {
+    //essentially this catches any error that gets thrown
+    //in any of the children of this error boundary component.
+    return { hasErrored: true };
+  }
+
+  componentDidCatch(error, info) {
+    console.log(error);
+  }
+
+  render() {
+    if (this.state.hasErrored) {
+      return (
+        <ErrorImageOverlay>
+          <ErrorImageContainer imageUrl="https://i.imgur.com/U3vTGjX.png" />
+          <ErrorImageText>Sorry this page is broken</ErrorImageText>
+        </ErrorImageOverlay>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
